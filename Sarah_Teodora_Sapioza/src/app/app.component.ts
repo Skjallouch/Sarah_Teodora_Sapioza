7a493e07c0ffbe7cb73b37db4f1afaf2104fc677
teodoraistrate <teodoraistrate63@yahoo.com>
2025-01-28 14:21:48 +0100

    premiere version footer

--- Sarah_Teodora_Sapioza/src/app/app.component.ts
+++ Sarah_Teodora_Sapioza/src/app/app.component.ts
@@ -15,11 +15,12 @@ export class AppComponent {
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from './shared/components/header/header.component';
+import {FooterComponent} from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent], // Nécessaire pour standalone
+  imports: [RouterOutlet, HeaderComponent, FooterComponent], // Nécessaire pour standalone
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
